b3068ce731fdd82bf3a8438c393df3d9ab7d6b75
srushti-2005 <150372082+srushti-2005@users.noreply.github.com>
2025-02-22 17:17:55 +0530

    Add files via upload

---
+++ audience.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const filterButton = document.querySelector('button');
+    filterButton.addEventListener('click', function() {
+        alert('Filters applied!'); 
+    });
+});
