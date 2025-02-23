ebaac4f6dedf994db094436830900a012a1d0c31
srushti-2005 <srushtiddesai@gmail.com>
2025-02-23 06:52:11 +0530

    delete

--- audience.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    
-    const filterButton = document.querySelector('button');
-    filterButton.addEventListener('click', function() {
-        alert('Filters applied!'); 
-    });
-});
